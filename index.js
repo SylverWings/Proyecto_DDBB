a78258487caf4d08dc2cec2fa9a29cf08a529c51
Lionel <Lionel5garcia@gmail.com>
2022-06-17 15:49:14 +0200

    Instaladas las librerias de npm

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const express = require ('express');
+const colors = require('colors');
+
+const app = express ();
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=> console.log(`Server on port ${PORT}`.bgGreen.black));
